4eedfa3845e11dfee95639c608560cb3da80f8a4
Martin Tsvetkov <sc19mmt@leeds.ac.uk>
2022-01-29 12:00:21 +0000

    2D cone lighting

---
+++ Assets/Scripts/CoreMechanics/LightSourceDetection.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightSourceDetection : MonoBehaviour
+{
+
+    public static float angle = 60.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
