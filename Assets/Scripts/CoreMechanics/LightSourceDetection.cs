a684136d3d96283fe1ba2911e0796dbf7ea79945
Martin Tsvetkov <sc19mmt@leeds.ac.uk>
2022-01-29 12:58:25 +0000

    Raycast Detection

--- Assets/Scripts/CoreMechanics/LightSourceDetection.cs
+++ Assets/Scripts/CoreMechanics/LightSourceDetection.cs
@@ -1,20 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class LightSourceDetection : MonoBehaviour
 {
 
-    public static float angle = 60.0f;
+    [SerializeField] private Collider2D playerCollider;
+    [SerializeField] private float angle = 60.0f;
+    [SerializeField] private Light2D lightObject;
     // Start is called before the first frame update
     void Start()
     {
-
+        lightObject.pointLightOuterAngle = angle;
     }
 
     // Update is called once per frame
     void Update()
     {
 
+
+        Bounds playerBounds = playerCollider.bounds;
+        Vector3[] bboxCorners = {
+            new Vector3(playerBounds.extents.x, playerBounds.extents.y),
+            new Vector3(playerBounds.extents.x, -playerBounds.extents.y),
+            new Vector3(-playerBounds.extents.x, playerBounds.extents.y),
+            new Vector3(-playerBounds.extents.x, -playerBounds.extents.y),
+        };
+
+        for (int i = 0; i < 4; i++)
+        {
+            Vector3 playerDirection = playerCollider.transform.position - transform.position - bboxCorners[i];
+            if (Vector3.Angle(playerDirection, -transform.up) <= angle / 2)
+            {
+                RaycastHit2D hit = Physics2D.Raycast(transform.position, playerDirection);
+                if (hit.collider == playerCollider)
+                {
+                    hitReaction();
+                    return;
+                }
+            }
+        }
+        lightObject.color = Color.white;
+    }
+
+    private void hitReaction()
+    {
+        //change color to red for now
+        lightObject.color = Color.red;
     }
 }
